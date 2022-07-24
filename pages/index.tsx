907ea129660bb79cf6395fa448a5c89c525f0fef
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2022-07-24 23:33:43 +0900

    デザイン微修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,9 +18,9 @@ const IndexPage = (props: IndexProps) => {
   const { setting, works, backendskills, frontendskills } = props
   return (
     <>
-      <section id="about">
+      <section id="about" className="mt-12">
         <h2 className="font-fancy	text-center text-3xl">About</h2>
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div className="container mx-auto flex px-5 py-6 md:flex-row flex-col items-center">
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
             <Image src={setting.aboutImage.url} layout="responsive"
                   width={720}
