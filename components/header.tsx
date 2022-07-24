4026279e0e5e877b7f27efdb30a450abfeb2568b
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2022-07-24 17:08:08 +0900

    ヘッダーのロゴをクリックでTOPページに遷移

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header>
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <span className="ml-3 text-xl font-fancy">My Portfolio</span>
-        </a>
+        <Link href="/">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <span className="ml-3 text-xl font-fancy">My Portfolio</span>
+          </a>
+        </Link>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <a className="mr-5 hover:text-gray-900">Home</a>
           <a className="mr-5 hover:text-gray-900">About</a>
