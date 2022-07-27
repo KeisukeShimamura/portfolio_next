f93908470b4fa05f90ffb64bd866846d1dd2b5a6
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2022-07-27 22:46:24 +0900

    スキルバーを崩れずに表示

--- components/SkillBarList.tsx
+++ components/SkillBarList.tsx
@@ -7,13 +7,13 @@ type Props = {
 }
 
 const SkillBarList = ({ skills, title, reverse=false }: Props) => (
-  <div className="md:w-1/2 w-full md:pl-2">
+  <div className="md:w-1/2 w-full md:pl-2 my-6">
     <h3 className="text-center font-medium mb-2">{title}</h3>
     <div className="flex flex-col">
       {
         skills.map((skill) => 
           <div key={skill.id} className="relative h-10 bg-gray-100 mb-6 rounded">
-            <div className={`animate-graph${skill.score} h-10 absolute ${reverse ? 'right-0 flex-row-reverse' : 'left-0'} top-0 bottom-0 m-auto bg-indigo-500 flex items-center justify-between md:px-6 px-2 rounded`}>
+            <div className={`w-[${skill.score}%] h-10 absolute ${reverse ? 'md:right-0 md:left-auto right-auto left-0 md:flex-row-reverse flex-row' : 'left-0'} top-0 bottom-0 m-auto bg-indigo-500 flex items-center justify-between md:px-6 px-2 rounded`}>
               <span className="text-white md:text-base text-sm">{skill.name}</span>
               <span className="text-white md:text-base text-sm">{skill.score}</span>
             </div>
@@ -21,6 +21,12 @@ const SkillBarList = ({ skills, title, reverse=false }: Props) => (
         )
       }
     </div>
+    <div className="w-[50%] hidden"></div>
+    <div className="w-[60%] hidden"></div>
+    <div className="w-[70%] hidden"></div>
+    <div className="w-[80%] hidden"></div>
+    <div className="w-[90%] hidden"></div>
+    <div className="w-[100%] hidden"></div>
   </div>
 )
 
